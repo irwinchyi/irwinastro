f0a2c2d122f6f3bd33a68dc135330e2bc3d9efc6
Irwin Qi <irwinchyi@live.com>
2025-04-29 13:49:41 -0400

    Revert "push"
    This reverts commit a569f4a81ed97f5d09869602f7959c8d7f5dce87.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,27 +1,30 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
+
 import netlify from '@astrojs/netlify';
 import cloudflare from '@astrojs/cloudflare';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [starlight({
-      title: 'Irwin&#39;s Doc',
-      social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/withastro/starlight' }],
-      sidebar: [
-          {
-              label: 'Comp Comm',
-            autogenerate: { directory: 'comp' },
-        
-          },
-          {
-              label: 'Reference',
-              autogenerate: { directory: 'reference' },
-          },
-      ],
-      }
-  ), ],
+  integrations: [
+      starlight({
+          title: 'Irwins Doc',
+          social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/withastro/starlight' }],
+          sidebar: [
+              {
+                  label: 'Comp Comm',
+                autogenerate: { directory: 'comp' },
+            
+              },
+              {
+                  label: 'Reference',
+                  autogenerate: { directory: 'reference' },
+              },
+          ],
+          }
+      )
+	],
 
   adapter: netlify(),
   adapter: cloudflare(),
